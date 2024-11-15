e0195578bca480b8a8e97c39060fac94bc68fad2
Max <rusin.m@gitflic-team.ru>
2024-11-15 16:51:34 +0300

    Добавление входа на гитхаб

---
+++ tests/conftest.py
@@ -0,0 +1,49 @@
+import pytest_asyncio
+from dotenv import load_dotenv
+from playwright.async_api import async_playwright
+import os
+
+@pytest_asyncio.fixture(scope="function", autouse=True)
+def load_env():
+    load_dotenv()
+
+@pytest_asyncio.fixture(scope="session")
+async def browser_context():
+    load_dotenv()
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=True)
+        context = await browser.new_context()
+        base_url = 'https://github.com/login'
+        page = await context.new_page()
+        await page.goto(base_url)
+
+        # Логинимся и сохраняем cookies
+        login = os.getenv('LOGIN')
+        password = os.getenv('PASSWORD')
+
+        if not login or not password:
+            raise ValueError("Переменные LOGIN и PASSWORD должны быть определены в .env")
+
+        await page.fill("#login_field", login)
+        await page.fill("#password", password)
+        await page.get_by_role("button", name="Sign in").click()
+        await page.wait_for_timeout(2000)
+        cookies = await context.cookies()
+
+        await browser.close()
+        return cookies
+
+# Фикстура для браузера с cookies
+@pytest_asyncio.fixture(scope="function")
+async def browser_with_cookies(browser_context):
+    """Открывает браузер с сохраненными cookies"""
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=False)
+        context = await browser.new_context()
+        await context.add_cookies(browser_context)
+        page = await context.new_page()
+        try:
+            yield page
+        finally:
+            await context.close()
+            await browser.close()
---
+++ tests/test_login.py
@@ -0,0 +1,11 @@
+import time
+
+import pytest
+from playwright.async_api import async_playwright
+
+@pytest.mark.asyncio
+async def test_authenticated_page(browser_with_cookies):
+    """Тест аутентифицированной страницы"""
+    page = browser_with_cookies
+    await page.goto("https://github.com/")
+    time.sleep(10)
